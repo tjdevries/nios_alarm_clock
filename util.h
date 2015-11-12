7dff6886a26220f65ccb94c1766aca4954cbf458
TJ DeVries <timothydvrs1234@gmail.com>
2015-11-12 14:28:23 -0500

    Fix compiler warnings
    Either added new lines or fixed types through the files

--- util.h
+++ util.h
@@ -8,7 +8,8 @@
 #include "hex.h"
 #include "timers.h"
 
-void write_time_to_buffer(char *time, int second, int minute, int hour);
+void write_time_to_buffer(char *time, int second, int minute, int hour, int am_pm_mode);
+
 void update_time(char * top_row, int *old_tenths_ptr, volatile int *tenths_ptr, int *seconds_ptr, int *minutes_ptr, int *hours_ptr, int *day, int *month, int *year, int am_pm_mode, int tenths_displayed);
 void update_hour(char * top_row, int *hours_ptr, int *day, int *month, int *year, int am_pm_mode);
 void update_min(char * top_row, int *minutes_ptr, int *hours_ptr, int *day, int *month, int *year, int am_pm_mode);
@@ -19,6 +20,8 @@ void increment_minutes(char *time, int *minutes);
 void increment_seconds(char *time, int *seconds);
 
 void increment_date(int *day, int *month, int *year);
-void is_leap_year(int year);
+int is_leap_year(int year);
 
 void is_in_array(int val, int *arr, int size);
+
+void handle_am_pm(char * time, int *hours_ptr, int am_pm_mode);

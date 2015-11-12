c1da216393fd62046146ad086d3460e674d54945
TJ DeVries <timothydvrs1234@gmail.com>
2015-11-12 14:40:18 -0500

    Reformat include and add irq_register
    This eliminates all the compiler warnings at this time

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
+
+#include <stdio.h>
+#include <string.h>
+
+// Support includes
+#include "BSP/system.h"
+#include "BSP/HAL/src/alt_irq_register.c"
 #include "altera_up_avalon_character_lcd.h"
 #include "altera_avalon_pio_regs.h"
 #include "altera_avalon_timer_regs.h"
-#include <stdio.h>
-#include <string.h>
-#include "BSP/system.h"
 
 // Personal includes
 #include "hex.h"
